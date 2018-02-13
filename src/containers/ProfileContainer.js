a962ed4be175780e51e2910f880f866d0b48566e
van <iclposadas@gmail.com>
2018-02-13 13:41:02 +0800

    updated information

--- src/containers/ProfileContainer.js
+++ src/containers/ProfileContainer.js
@@ -1,43 +1,52 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import Header from '../components/Header'
-import Sidebar from '../components/Sidebar'
+import React from "react";
+import { Link } from "react-router-dom";
+import Header from "../components/Header";
+import Sidebar from "../components/Sidebar";
 
 const ProfileContainer = () => (
   <div className="profile">
-    <Header/>
+    <Header />
     <div className="container">
       <Sidebar />
       <div id="content" className="content container column">
         <p>I came from the City of Pines</p>
         <p>I live in Taguig City</p>
         <p>I watch a lot of tv series</p>
+        <p>I am currently studying 日本語 (Japanese)</p>
         <p>
-          I work as a developer, some <strong>web</strong>, some{' '}
+          I do lots of stuff as a developer, some <strong>web</strong>, some{" "}
           <strong>mobile</strong>, some all around
         </p>
-        <p>I love to learn new tech thingies</p>
+        <p>I love to learn new things and makes me excited</p>
         <p>
-          I was as a dev support at <strong>Texas Instruments Inc.</strong>
+          I started my career as a dev/support at{" "}
+          <strong>Texas Instruments Inc.</strong>
         </p>
         <p>
-          I then worked for <strong>HP</strong>/<strong>HPE</strong>/<strong>DXC</strong>
+          I then worked for <strong>DXC Technology</strong>{" "}
+          <i>(formerly Hewlett Packard)</i>
         </p>
         <p>
           I worked with the Microsoft Practice Team involved in Microsoft Stack
           of Technologies
         </p>
-        <p>I am now a developer at Lotus Labs Inc. </p>
         <p>
-          I made a somewhat resume{' '}
+          I transfered to <strong>Lotus Labs Inc.</strong> and worked as a front
+          end web developer{" "}
+        </p>
+        <p>
+          I am now working as a "Software Engineer" in <strong>Infor</strong>
+        </p>
+        <p>
+          I made a somewhat resume{" "}
           <Link to="/resume">
             <em>"here"</em>
-          </Link>{' '}
+          </Link>{" "}
           if you prefer those things
         </p>
       </div>
     </div>
   </div>
-)
+);
 
-export default ProfileContainer
+export default ProfileContainer;
--- src/containers/ResumeContainer.js
+++ src/containers/ResumeContainer.js
@@ -9,13 +9,17 @@ import csharp from '../images/csharp.png'
 
 const ResumeContainer = () => (
   <div className="resume">
-  <Header />
+    <Header />
     <h1 className="title">Key Points</h1>
     <h2>Experience</h2>
     <ul className="experience-list">
       <li className="experience">
-        <h3>Lotus Labs Inc. - Web Developer</h3>
-        <h4>August 2017 - Present</h4>
+        <h3>Infor - Software Engineer</h3>
+        <h4>January 2018 - Present</h4>
+      </li>
+      <li className="experience">
+        <h3>Lotus Labs Inc. - Front End Web Developer</h3>
+        <h4>August 2017 - January 2018</h4>
       </li>
       <li className="experience">
         <h3>Hewlett Packard/ HPE/ DXC - Service Information Developer</h3>
@@ -28,23 +32,23 @@ const ResumeContainer = () => (
     </ul>
     <h2>Technical Skills</h2>
     <ul className="skill-list">
-    <li className="skill">
-        <img src={csharp}  alt="c#"/>
+      <li className="skill">
+        <img src={csharp} alt="c#" />
       </li>
       <li className="skill">
-        <img src={react} alt="reactjs"/>
+        <img src={react} alt="reactjs" />
       </li>
       <li className="skill">
-        <img src={angular} alt="angularjs"/>
+        <img src={angular} alt="angularjs" />
       </li>
       <li className="skill">
-        <img src={javascript} alt="javascript"/>
+        <img src={javascript} alt="javascript" />
       </li>
       <li className="skill">
-        <img src={html5} alt="html5"/>
+        <img src={html5} alt="html5" />
       </li>
       <li className="skill">
-        <img src={css3} alt="css3"/>
+        <img src={css3} alt="css3" />
       </li>
     </ul>
     <h2>Education</h2>
